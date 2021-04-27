a5539c5a4f23c3bd794413a608d8bad9713d60c9
Bryan Lunt <bjlunt2@illinois.edu>
2021-04-27 17:23:19 -0500

    added script to run docker container.

---
+++ docker_run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+###Snippet from http://stackoverflow.com/questions/59895/
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+###end snippet
+
+IMAGE_NAME="luntlab/cs547_project:latest"
+
+
+docker run -it --rm -v${SCRIPT_DIR}:/workspace/project ${IMAGE_NAME}
