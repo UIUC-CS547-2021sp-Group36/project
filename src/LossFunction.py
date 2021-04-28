7ae2b0234446ccb7dd1c54ef2038d1d5ac2f7f34
Bryan Lunt <bjlunt2@illinois.edu>
2021-04-28 14:45:46 -0500

    Added credit and TODO about citation.

--- src/LossFunction.py
+++ src/LossFunction.py
@@ -1,3 +1,7 @@
 import torch.nn
 
+#Thanks to Askar for finding this.
+#TODO: Make sure to cite the underlying paper in our writeup.
+
+#https://pytorch.org/docs/stable/generated/torch.nn.TripletMarginLoss.html
 LossFunction = torch.nn.TripletMarginLoss
