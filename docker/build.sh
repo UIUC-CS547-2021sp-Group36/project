767ff5d3017c8efeb9b99835286021f0b7896e9e
Bryan Lunt <bjlunt2@illinois.edu>
2021-04-27 21:56:16 -0500

    better docker scripts

--- docker/build.sh
+++ docker/build.sh
@@ -3,3 +3,7 @@
 ###Snippet from http://stackoverflow.com/questions/59895/
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ###end snippet
+
+docker build -t luntlab/cs547_project:latest --target project_base ${SCRIPT_DIR}
+
+#docker push luntlab/cs547_project:latest
