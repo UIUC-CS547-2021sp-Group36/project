d4ac43da2dfa087b6c0d4dcf4ae740347ba1fd07
Bryan Lunt <bjlunt2@illinois.edu>
2021-04-29 22:57:57 -0500

    test image first

--- src/validate_dataset.py
+++ src/validate_dataset.py
@@ -1,6 +1,11 @@
 import ImageLoader
 
 if __name__ == "__main__":
+    
+    import torchvision.datasets
+    print("Atempt to load offending image")
+    torchvision.datasets.folder.default_loader("/workspace/datasets/tiny-imagenet-200/train/n03980874/images/n03980874_480.JPEG")
+
     print("load data")
     all_train = ImageLoader.load_imagefolder("/workspace/datasets/tiny-imagenet-200/train")
     
