06c3a95d9dfbe20f4997083a2f9bbfed835a808a
Aidana Karipbayeva <aidana2@illinois.edu>
2021-05-04 22:25:25 -0500

    New model added

--- src/models/Resnet18FrozenWrapper.py
+++ src/models/Resnet18FrozenWrapper.py
@@ -10,11 +10,11 @@ class Resnet18FrozenWrapper(torch.nn.Module):
     def __init__(self, output_dimension=300,internal_dimension=500):
         super(Resnet18FrozenWrapper, self).__init__()
         self.resnet = tvmodels.resnet18(pretrained=True)
-        
+
         #We find the dimensions of the output from resnet
         #that will be the dimension of the input for the subesquent layer
         n_resnetout = list(self.resnet.children())[-1].out_features
-        
+
         self.additional_layers = torch.nn.Sequential(
             torch.nn.Linear(n_resnetout, internal_dimension),
             torch.nn.Sigmoid(),
@@ -22,39 +22,39 @@ class Resnet18FrozenWrapper(torch.nn.Module):
             torch.nn.Sigmoid()
             #torch.nn.Softmax(dim=1) #More classifier like
         )
-        
+
         #FREEZING (search other files)
         #This is supposed to help freeze the submodel, but the optimizer
         #does not respect this alone. It's very sad.
         self.resnet.requires_grad = False
-    
+
     def forward(self, images):
         rn_embed = self.resnet(images)
         output = self.additional_layers(rn_embed)
-        
+
         #Normalize output while training
         #Nevermind, we'll use a loss function to enforce that
         #if self.training:
         #    output = torch.nn.functional.normalize(output,dim=1)
-        
+
         return output
 
 
 if __name__ == "__main__":
     import ImageLoader
-    
+
 
     print("load data")
     all_train = ImageLoader.load_imagefolder()
     train, val = ImageLoader.split_imagefolder(all_train, [0.9,0.1])
-    
-    
+
+
     resnet18 = models.resnet18(pretrained=True)
-    
+
     #TODO figure out fake batch creation, see pseudocode
-    
+
     fake_batch = all_train[0][0].unsqueeze(0) #fake batch of one image
-    
+
     one_set_of_embeddings = resnet18.forward(fake_batch) #the unsqeeze is because resnet only wants batches.
-    
+
     #check about the properties of one_set_of_embeddings
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -5,14 +5,16 @@ import torchvision.models as models
 import torchvision.models as tvmodels
 
 from .Resnet18FrozenWrapper import Resnet18FrozenWrapper
-
+from .NewModel import NewModel
 
 def create_model(model_name="dummy"):
     if "resnet18" == model_name:
         return tvmodels.resnet18(pretrained=True)
+    elif "newModel" == model_name:
+        return NewModel()
     elif "dummy" == model_name:
         return Resnet18FrozenWrapper()
-    
+
     #TODO: Add options for other models as we implement them.
-    
+
     raise Exception("No Model Specified")
