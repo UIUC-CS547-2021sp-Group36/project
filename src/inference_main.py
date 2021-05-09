2f740a549b2f55f735fe82952f823ac350d9c75b
Bryan Lunt <bjlunt2@illinois.edu>
2021-05-09 15:01:02 -0500

    Inference program formats nicely with a header.

--- src/inference_main.py
+++ src/inference_main.py
@@ -7,6 +7,25 @@ import torch
 import models
 import data.ImageLoader as ImageLoader
 
+def pairwise_distances(matrix_of_rows, epsilon=1.0e-6):
+    """
+    Computes the pairwise_distances between all pairs of rows.
+    
+    Returns a square matrix. Diagonal will be zeros.
+    """
+    
+    #TODO: replace with scipy.spatial.distance.pdist
+    #DOCS https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.pdist.html#scipy.spatial.distance.pdist
+    
+    b = numpy.dot(matrix_of_rows,matrix_of_rows.T)
+    d = numpy.diag(b)
+    c_input = d + d.reshape(-1,1) - 2.0*b
+    #numpy doesn't like negative inputs to power with fractional power.
+    #UGLY # HACK
+    c_input += epsilon
+    c = numpy.power(c_input, 0.5)
+    return c
+
 def nonneg_int(i):
     ival = int(i)
     if ival < 0:
@@ -27,6 +46,8 @@ def main():
     
     arg_parser.add_argument("--out",type=str,required=True)
     arg_parser.add_argument("--best_matches",type=str,default=None)
+    arg_parser.add_argument("--n_best",type=int,default=10)
+    arg_parser.add_argument("--n_worst",type=int,default=10)
     
     args = arg_parser.parse_args()
     #print(args)
@@ -60,23 +81,39 @@ def main():
     #HDF5 would be much better.
     
     if args.best_matches is not None:
-        with open(args.best_matches,"w") as outfile:
+        with open(args.best_matches,"w") as matches_outfile:
             #These three lines find the Euclidean distance between all vectors.
-            b = numpy.dot(embeddings,embeddings.T)
-            d = numpy.diag(b)
-            c = numpy.power(d + d.reshape(-1,1) - 2.0*b, 0.5)
+            c = pairwise_distances(embeddings)
             numpy.fill_diagonal(c,c.max() + 10.0)#prevents becoming own best match. #self distance is always 0
-            m = c.argmin(1)
+            
+            m = c.argsort(1)
+            
+            m_best  = m[:,:args.n_best]
+            m_worst = m[:,-args.n_worst:]
             
             #TODO: This only gets the single best match. We need the best several.
             #Can accomplish with argsort.
             
-            for i, (closest, (query_path, query_label)) in enumerate(zip(m, test_data.samples)):
+            #TODO: Write a header line
+            header_line = "#QUERY\tQ_LABEL\t"
+            header_line += "\t".join(["BEST_{}\tBEST_LABEL_{}".format(i,i) for i in range(args.n_best)])
+            header_line += "\t" + "\t".join(["WORST_{}\tWORST_LABEL_{}".format(i,i) for i in range(args.n_worst)][::-1])
+            print(header_line,file=matches_outfile)
+            
+            for i, (closest, furthest, (query_path, query_label)) in enumerate(zip(m_best, m_worst, test_data.samples)):
                 query_filename = os.path.basename(query_path)
                 
-                closest_path, closest_label = test_data.samples[closest]
-                closest_filename = os.path.basename(closest_path)
-                print(query_filename,query_label,closest_filename,closest_label,file=outfile)
+                bn_label = lambda samp:(os.path.basename(samp[0]),samp[1])
+                closest_fns_label = [bn_label(test_data.samples[c]) for c in closest]
+                furthest_fns_label = [bn_label(test_data.samples[f]) for f in furthest]
+                
+                output_chunks = "\t".join(["{}\t{}".format(fn,l) for fn,l in closest_fns_label])
+                output_chunks += "\t" + "\t".join(["{}\t{}".format(fn,l) for fn,l in furthest_fns_label])
+                
+                print(query_filename,query_label,output_chunks,
+                        sep="\t",
+                        file=matches_outfile
+                      )
     
     
 if __name__ == "__main__":
