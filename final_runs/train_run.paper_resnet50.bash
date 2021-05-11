2d8901114144fbb972c36de4677624f051288883
Bryan J. Lunt <bjlunt2@illinois.edu>
2021-05-11 00:29:25 -0500

    fixed final runs files

--- final_runs/train_run.paper_resnet50.bash
+++ final_runs/train_run.paper_resnet50.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -l walltime=20:30:0,nodes=1:xk:ppn=16,gres=shifter
-#PBS -N paper_resnet50
+#PBS -N final.paper_resnet50
 
 cd ${PBS_O_WORKDIR}
 
@@ -17,4 +17,4 @@ aprun -b -n 1 -d 16  -- shifter --image=docker:luntlab/cs547_project:latest \
     --module=mpich,gpu -- python src/train_main.py -v \
     ${RESUME_ARGS} \
     --wandb_tags debug,bw,final --epochs 1000 ${MODEL_ARGS} \
-    --loss default --lr 0.001 --margin 2.0 --batch_size 20
+    --loss default --lr 0.001 --margin 2.0 --batch_size 10
